7c5d2c6a2ddc8a7014b84c123019ff8f68860b03
Anthony Landreth <anthony.w.landreth@gmail.com>
2020-05-03 19:19:52 -0400

    flash card script creates pre-linked cards

--- new_flash_card.sh
+++ new_flash_card.sh
@@ -2,10 +2,20 @@
 
 # Assumes all files will be named in snake case
 d=$(date +'%y%m%s')
-n="${d}_$1.md"
-touch $n
+q="${d}_$1_question.md"
+a="${d}_$1_answer.md"
+touch $q
+touch $a
 
-echo "#flash_card ${d}" >> $n
-echo "" >> $n
-echo "" >> $n
-echo "[[${n}]]" >> $n
+echo "#flash_card ${d}" >> $q
+echo "" >> $q
+echo "" >> $q
+echo "[[${a}]]" >> $q
+echo "[[${q}]]" >> $q
+
+echo "#flash_card ${d}" >> $a
+echo "" >> $a
+echo "" >> $a
+echo "[[${q}]]" >> $a
+echo "[[${a}]]" >> $a
+
